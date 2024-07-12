f89c6a2e6fe792bb8fe050ee088a56e8096a8730
Juan Antonio Hernández <124842903+programadorJ25@users.noreply.github.com>
2024-07-12 11:34:17 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// En base a las instrucciones, deberas crear una función que identifique si hay elementos repetidos en los arreglos e imprimir en consola si es veradero o falso
+
+const a = [1, 2, 3, 4, 4, 3, 3];
+const b = ["romida", "ROMIDA", "test"];
+const c = [2, 5, 7, 8, 11];
---
+++ test.js
@@ -0,0 +1,18 @@
+//Realiza un cambio de variables de los siguientes valores e imprimir en consola.
+
+let X = 5;
+let y = 7;
+
+//Una vez realizado el cambio de variables debes crear una función que muestre el valor
+//de la posición "n" en la secuencia fibonacci
+//0+0=0
+//0+1=1
+//1+1=2
+//2+1=3
+//3+2=5
+//5+3=8
+//8+5=13
+
+//Una vez terminada la funnción, deberas crear un loggin en js, html y css (puedes usar bootstrap); con validación de usuario, además de poder
+//crear un nuevo usuario, debera poder ocultar o mostrar la contraseña, comparar la confirmación de la contraseña, validar si el correo ha sido registrado;
+//esto debe ir a compaño de la base de datos.
